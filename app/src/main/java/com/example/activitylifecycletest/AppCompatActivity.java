6fb6a75c7443b8d9771ee9c1b41d81ec7080d095
Zhusurong <2037288572@qq.com>
2017-01-13 21:35:08 +0800

    Initial commit

---
+++ app/src/main/java/com/example/activitylifecycletest/AppCompatActivity.java
@@ -0,0 +1,12 @@
+package com.example.activitylifecycletest;
+
+import android.os.Bundle;
+
+/**
+ * Created by 20372 on 2017/1/13.
+ */
+public class AppCompatActivity {
+    public void onCreate(Bundle savedInstanceState) {
+
+    }
+}
